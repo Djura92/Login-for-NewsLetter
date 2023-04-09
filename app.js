7ca6bd183092297ee808c192068c2b8c6d4ea340
Your Name <you@example.com>
2023-04-09 14:06:42 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,75 @@
+//jshint esversion: 6
+
+  const express = require('express');
+  const bodyParser = require('body-parser');
+  const request = require('request');
+  const https = require('https');
+  const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+  //  eb00808aeb
+  //26aeb6564238662c1003c8bba2fbbeef-us18
+
+  const options = {
+    method:"POST",
+    auth: "djura:26aeb6564238662c1003c8bba2fbbeef-us18"
+  };
+  const app = express();
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(express.static('public'));
+
+  app.get('/', function(req, res) {
+    console.log("refreshed");
+    res.sendFile(__dirname + "/singup.html");
+  });
+
+  app.post('/', function(req, res){
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const eMail = req.body.email;
+
+    const data = {
+      members: [
+        {
+          email_address: eMail,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+          }
+        }
+      ]
+    }
+    const jsonData = JSON.stringify(data);
+
+    const url = 'https://us18.api.mailchimp.com/3.0/lists/eb00808aeb';
+
+    const options = {
+      method: "POST",
+      auth: "Nikola:26aeb6564238662c1003c8bba2fbbeef-us18"
+    }
+
+    const request = https.request(url, options, function(response){
+      if (response.statusCode === 200) {
+          res.sendFile(__dirname + "/success.html");
+      }else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+
+
+      response.on("data", function(data){
+        console.log(JSON.parse(data));
+      });
+    });
+    request.write(jsonData);
+    request.end();
+
+  });
+
+  app.post("/failure", function(req, res){
+    res.redirect("/");
+  });
+
+
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("listening port: 3000");
+  });
